193070c211a19c0dd8923c9c7eb44ab3381828a1
Jacson <jacsonsavio7@gmail.com>
2022-11-29 15:41:11 -0300

    Melhorias no perfil de usuário

--- app/pages/profile/profile.js
+++ app/pages/profile/profile.js
@@ -19,3 +19,37 @@
 
   document.querySelector(".list").innerHTML = cardString;
 })();
+
+let testte = localStorage.getItem("teste");
+$(".profile-pic").attr("src", testte);
+
+$(document).ready(function () {
+  var readURL = function (input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $(".profile-pic").attr("src", e.target.result);
+        localStorage.setItem("teste", reader.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+
+  $(".file-upload").on("change", function () {
+    readURL(this);
+  });
+
+  $(".upload-button").on("click", function () {
+    $(".file-upload").click();
+  });
+});
+
+$("#image-profile").on("mouseover", function () {
+  $(".profile-pic").css("opacity", "0.5");
+});
+
+$("#image-profile").on("mouseout", function () {
+  $(".profile-pic").css("opacity", "1");
+});
